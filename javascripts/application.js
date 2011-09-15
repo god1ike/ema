23898c22934a185690f6518087782e91ccd93091
vad <vad4msiu@gmail.com>
2011-09-15 13:05:34 +0400

    Все наеблося. Надо сделать что бы второй бородатый мужик был сверху или снизу и его не было видно

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,11 +1,16 @@
 $(document).ready(function() {
     $('div.part').live("hover",
     function() {
-        if (!$(this).hasClass("active")) {
-            $('div.part.active p.description').slideUp("fast");
-            $(this).find(' p.description').slideDown("fast");
-            $('div.part.active').removeClass("active");
-            $(this).addClass("active");
+        if (!$(this).hasClass("active") && !$("div.part p.description").is(":animated")) {
+            var tmp = this;
+            $('div.part.active p.description').slideUp("fast",
+            function() {
+                $('div.part.active').removeClass("active");
+            });
+            $(this).find('p.description').slideDown("fast",
+            function() {
+                $(tmp).addClass("active");
+            });
         }
     });
 });
