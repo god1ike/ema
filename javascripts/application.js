78ab8c3a51f40d7d92685f92a0625100b241f7b0
god1ike <shakirov.ilya@gmail.com>
2011-09-16 15:30:16 +0400

    Багфикс лишних пикселей в партблоге

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,6 +1,14 @@
+<<<<<<< HEAD
 // window.onload = function() {
 //     $('div.parts').css("height", $('div.blog').height());
 // }
+=======
+/*
+window.onload = function() {
+    $('div.parts').css("height", $('div.blog').height());
+}
+*/
+>>>>>>> Багфикс лишних пикселей в партблоге
 
 
 $(document).ready(function() {
